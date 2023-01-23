c648ed7d2d2cd3bd116c2114d9a78b63296592ed
Rahul Kumar jha <rahul.jha@gartner.com>
2023-01-23 17:25:39 +0530

    Add project files.

---
+++ Authorization.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Read_sheet_data_api
+{
+    public class Authorization
+    {
+        [Key]
+        public string UserId { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecrert { get; set; }
+        public string[] Scopes { get; set; }
+    }
+}
---
+++ Controllers/GoogleSheetController.cs
@@ -0,0 +1,67 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Read_sheet_data_api.Controllers
+{
+    [Route("api/[controller]")]
+    public class GoogleSheetController : ControllerBase
+    {
+        private readonly ILogger<GoogleSheetController> _logger;
+
+        public GoogleSheetController(ILogger<GoogleSheetController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get(string sheetId, string range, string accessToken)
+        {
+            SheetsService sheetsService = new SheetsService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = GoogleCredential.FromAccessToken(accessToken),
+                ApplicationName = "Google-SheetsSample/0.1",
+            });
+
+            string spreadsheetId = sheetId;
+
+
+            SpreadsheetsResource.ValuesResource.GetRequest.ValueRenderOptionEnum valueRenderOption = (SpreadsheetsResource.ValuesResource.GetRequest.ValueRenderOptionEnum)0;  // TODO: Update placeholder value.
+
+
+            SpreadsheetsResource.ValuesResource.GetRequest.DateTimeRenderOptionEnum dateTimeRenderOption = (SpreadsheetsResource.ValuesResource.GetRequest.DateTimeRenderOptionEnum)0;  // TODO: Update placeholder value.
+
+            SpreadsheetsResource.ValuesResource.GetRequest request = sheetsService.Spreadsheets.Values.Get(spreadsheetId, range);
+            request.ValueRenderOption = valueRenderOption;
+            request.DateTimeRenderOption = dateTimeRenderOption;
+
+            Google.Apis.Sheets.v4.Data.ValueRange response = request.Execute();
+
+            return Ok(response.Values);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Authorise(Authorization authorization )
+        {
+            UserCredential data = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                             new ClientSecrets
+                             {
+                                 ClientId = authorization.ClientId,
+                                 ClientSecret = authorization.ClientSecrert
+                             },
+                             authorization.Scopes,
+                             authorization.UserId,
+                             CancellationToken.None);
+
+            
+
+            return Ok(data.Token);
+        }
+
+    }
+}
